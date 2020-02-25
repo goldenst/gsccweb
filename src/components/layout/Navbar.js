214ba3fa222b65a4ce8fd2e2d045d1ef1fcad2bc
goldenst <jeff@goldenstatetow.com>
2020-02-25 13:55:16 -0800

    inital commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        navbar 
+      </div>
+    )
+  }
+}
+
+
+export default Navbar
